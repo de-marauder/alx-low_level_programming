6cdb58a98863e903ac074e36381c1bff1c2105a8
de-marauder <ezikegodson@gmail.com>
2022-07-01 16:21:57 -0700

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+/**
+ *main - print 0123456789
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int num = 0;
+
+while (num < 10)
+{
+printf("%i", num);
+num++;
+}
+putchar('\n');
+
+return (0);
+}
