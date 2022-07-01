d10e108691e953d767433766b32de5441ae1a3e4
de-marauder <ezikegodson@gmail.com>
2022-07-01 15:02:43 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  *main - Entry point
+  *Return: Zero always
+  */
+
+int main(){
+
+char al = 'a';
+
+while (al < n+25)
+{
+putchar(al);
+putchar("\n")
+}
+
+
+return (0)
+}
