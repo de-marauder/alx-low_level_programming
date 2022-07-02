4b332cbb8fcf23d62ff461a2827ad36a26eb7e3b
de-marauder <ezikegodson@gmail.com>
2022-07-01 17:59:58 -0700

    permute two 2 digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,20 +18,25 @@ for (ones = '0'; ones <= '9'; ones++)
 {
 for (t = tens; t <= '9'; t++) /*print second of pair*/
 {
-for (o = ones + 1; o <= '9'; o++)
+for (o = ones; o <= '9'; o++)
 {
+ if (!(t + o == tens + ones))
+ {
 putchar(tens);
 putchar(ones);
 putchar(' ');
 putchar(t);
 putchar(o);
 
-if (!((tens == '9' && ones == '8') &&
+ if (!((tens == '9' && ones == '8') &&
 (t == '9' && o == '9')))
 {
 putchar(',');
 putchar(' ');
 }
+ }
+
+
 }
 o = '0';
 }
