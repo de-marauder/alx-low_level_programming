997a5ba35d91a04c4b8e4cd9ca893f72ac3cfda6
de-marauder <ezikegodson@gmail.com>
2022-07-05 12:55:39 -0700

    print alphabets in lower case using custom functions

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,12 +7,11 @@
 
 void print_alphabet(void)
 {
-  char c = 'a';
-  int i;
-  for (i = c; i < c + 25; i++)
-    {
-      _putchar(i);
-    }
-  _putchar('\n');
-
+char c = 'a';
+int i;
+for (i = c; i <= c + 25; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
 }
