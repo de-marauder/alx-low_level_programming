d064c3a9ed35c4f9b1d2c95c1ca6897719c48ca5
de-marauder <ezikegodson@gmail.com>
2022-07-05 16:20:08 -0700

    Nested loops, print 24-hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,31 +7,31 @@
 
 void jack_bauer(void)
 {
-  int h_tens;
-  int h_units;
-  int m_tens;
+int h_tens;
+int h_units;
+int m_tens;
 int m_units;
 
- for (h_tens = 0; h_tens < 3; h_tens++)
-   {
-     for (h_units = 0; h_units <= 9; h_units++)
-       {
-	 for (m_tens = 0; m_tens < 6; m_tens++)
-	   {
-	     for (m_units = 0; m_units <= 9; m_units++)
-	       {
-		 _putchar(h_tens);
-		 _putchar(h_units);
-		 _putchar(':');
-		 _putchar(m_tens);
-		 _putchar(m_units);
+for (h_tens = 0; h_tens < 3; h_tens++)
+{
+for (h_units = 0; h_units <= 9; h_units++)
+{
+for (m_tens = 0; m_tens < 6; m_tens++)
+{
+for (m_units = 0; m_units <= 9; m_units++)
+{
+_putchar(h_tens + '0');
+_putchar(h_units + '0');
+_putchar(':');
+_putchar(m_tens + '0');
+_putchar(m_units + '0');
 
-		 if (h_tens == 2 && h_units == 3 && m_tens == 5 && m_units == 9)
-		   {
-		     return;
-		   }
-	       }
-	   }
-       }
-   }
+if (h_tens == 2 && h_units == 3 && m_tens == 5 && m_units == 9)
+{
+return;
+}
+}
+}
+}
+}
 }
