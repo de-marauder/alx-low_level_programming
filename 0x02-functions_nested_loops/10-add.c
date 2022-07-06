f39d5f7e99d0ef7c0e945ac8d365d2b4c71bbf59
de-marauder <ezikegodson@gmail.com>
2022-07-05 18:36:50 -0700

    Sum two numbers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,26 +1,6 @@
 #include "main.h"
 
 /**
- * _expo - calculates the exponent of @a to @b
- * @a: a number
- * @b: a number
- * Return: exponent of @a to @b
- */
-
-int _expo(int a, int b)
-{
-  int result = 1;
-  int i;
-  for (i = 0; i < b; i++)
-    {
-      result *= a;
-    }
-  
-  return (result);
-}
-
-
-/**
  * add - prints the sum of two numbers
  * @a: a number
  * @b: a number
@@ -29,7 +9,7 @@ int _expo(int a, int b)
 
 int add(int a, int b)
 {
-  int sum = a + b;
+int sum = a + b;
   
-  return (sum);
+return (sum);
 }
