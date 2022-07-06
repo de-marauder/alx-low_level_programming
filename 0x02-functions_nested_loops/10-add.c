e5401e5aa11e4ab2f54ed8996039a0920c78f8f1
de-marauder <ezikegodson@gmail.com>
2022-07-05 18:11:01 -0700

    print sum of two numbers using putchar

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <unistd.h>
 /**
  * add - prints the sum of two numbers
  * @a: a number
@@ -13,21 +13,22 @@ int add(int a, int b)
   int sum_copy = sum;
   int counter = 0;
 
+  write(1, sum, 4);
   /* count digits in sum */
-  while (sum_copy / 10 > 0)
+  /* while (sum_copy / 10 > 0)
     {
       sum_copy /= 10;
       counter += 1;
     }
-
+  */
   /* Print sum */
-  int i;
+  /* int i;
   for (i = counter + 1; i > 0; i--)
     {
       _putchar((sum / (10 ** counter)) + '0');
     }
   _putchar(sum % 10 + '0');
   _putchar('\n');
-  
-  return (sum)
+  */
+  return (sum);
 }
