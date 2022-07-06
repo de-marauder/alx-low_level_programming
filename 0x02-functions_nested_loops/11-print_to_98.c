a96203469df01e0e6dda22faa1a49175b84ee8ed
de-marauder <ezikegodson@gmail.com>
2022-07-05 19:43:12 -0700

    Print sequence from any integer n to 98 character wise

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,20 +9,20 @@
 
 void print_to_98(int n)
 {
-  while (n < 98)
-    {
-      printf("%d, ", n);
-      n++;
-    }
+while (n < 98)
+{
+printf("%d, ", n);
+n++;
+}
 
-  while (n > 98)
-    {
-      printf("%d, ", n);
-      n--;
-    }
+while (n > 98)
+{
+printf("%d, ", n);
+n--;
+}
 
-  if (n == 98)
-    printf("%d", n);
+if (n == 98)
+printf("%d", n);
 
-  printf("\n");
+printf("\n");
 }
