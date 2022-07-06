d7e2142715bb2832b46bc3c7eab4ad81adfae9ff
de-marauder <ezikegodson@gmail.com>
2022-07-06 00:55:11 -0700

    fibonacci sequence up to the 50th term

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Print the fibonacci sequence
+ * Return: zero always
+ */
+
+int main(void)
+{
+int a = 1;
+int b = 2;
+int c; /* Next number */
+int i; /* Loop variable */
+
+printf("%d, ", a);
+printf("%d, ", b);
+
+for (i = 2; i < 50; i++)
+{
+printf("%d, ", a + b);
+
+c = a + b;
+a = b;
+b = c;
+}
+printf('\n');
+}
