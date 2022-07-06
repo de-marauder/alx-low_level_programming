9da7e5ac97e8a77d96ec079e934e8e3de2d999d5
de-marauder <ezikegodson@gmail.com>
2022-07-05 18:24:12 -0700

    print sum of two numbers using putchar

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,5 +1,26 @@
 #include "main.h"
 #include <unistd.h>
+
+/**
+ * pow - calculates the exponent of @a to @b
+ * @a: a number
+ * @b: a number
+ * Return: exponent of @a to @b
+ */
+
+int pow(int a, int b)
+{
+  int result = 1;
+  int i;
+  for (i = 0; i < b; i++)
+    {
+      result *= a
+    }
+  
+  return (result)
+}
+
+
 /**
  * add - prints the sum of two numbers
  * @a: a number
@@ -10,25 +31,24 @@
 int add(int a, int b)
 {
   int sum = a + b;
-  /*  int sum_copy = sum;
+  int sum_copy = sum;
   int counter = 0;
-  */
-  write(1, &sum, 4);
+
   /* count digits in sum */
-  /* while (sum_copy / 10 > 0)
+ while (sum_copy / 10 > 0)
     {
       sum_copy /= 10;
       counter += 1;
     }
-  */
+
   /* Print sum */
-  /* int i;
+ int i;
   for (i = counter + 1; i > 0; i--)
     {
-      _putchar((sum / (10 ** counter)) + '0');
+      _putchar((sum / pow(10, counter)) + '0');
     }
   _putchar(sum % 10 + '0');
   _putchar('\n');
-  */
+
   return (sum);
 }
