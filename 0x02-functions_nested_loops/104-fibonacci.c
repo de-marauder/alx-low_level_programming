a3709304db13669872ae10ddb978497bc148cc84
de-marauder <ezikegodson@gmail.com>
2022-07-06 01:52:16 -0700

    print first 98 terms of fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Print the first 98 terms of fibonacci sequence
+ * Return: zero always
+ */
+
+int main(void)
+{
+long a = 1;
+long b = 2;
+long c; /* Next number */
+int i; /* Loop variable */
+
+printf("%ld, ", a);
+printf("%ld, ", b);
+
+for (i = 2; i < 98; i++)
+{
+c = a + b;
+
+printf("%ld", c);
+if (i != 97)
+{
+putchar(',');
+putchar(' ');
+}
+
+a = b;
+b = c;
+}
+printf("\n");
+return (0);
+}
