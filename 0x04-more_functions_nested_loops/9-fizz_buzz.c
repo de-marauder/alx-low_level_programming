26f84de249741360b2ce8663ec3e5ea9d89233b2
de-marauder <ezikegodson@gmail.com>
2022-07-07 15:19:11 -0700

    conditional printing of numbers 1-100

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,19 +1,58 @@
 #include "main.h"
-
 #include <unistd.h>
 
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: On success 1. On error, -1 is returned.
  */
 int _putchar(char c)
 {
-  return (write(1, &c, 1));
+return (write(1, &c, 1));
 }
 
+/**
+ * print_fizzbuzz - prints fizzbuzz
+ * Return: void
+ */
+
+void print_fizzbuzz(void)
+{
+_putchar('F');
+_putchar('i');
+_putchar('z');
+_putchar('z');
+_putchar('b');
+_putchar('u');
+_putchar('z');
+_putchar('z');
+}
+
+/**
+ * print_fizz - prints fizz
+ * Return: void
+ */
+
+void print_fizz(void)
+{
+_putchar('F');
+_putchar('i');
+_putchar('z');
+_putchar('z');
+}
+
+/**
+ * print_buzz - prints Buzz
+ * Return: void
+ */
+
+void print_buzz(void)
+{
+_putchar('B');
+_putchar('u');
+_putchar('z');
+_putchar('z');
+}
 
 /**
  * main - print 1 - 100. Perform special operation for multiples of 3 and 5
@@ -27,28 +66,15 @@ for (i = 1; i < 101; i++)
 {
 if (i % 3 == 0 && i % 5 == 0)
 {
-_putchar('F');
-_putchar('i');
-_putchar('z');
-_putchar('z');
-_putchar('b');
-_putchar('u');
-_putchar('z');
-_putchar('z');
+print_fizzbuzz();
 }
 else if (i % 3 == 0)
 {
-_putchar('F');
-_putchar('i');
-_putchar('z');
-_putchar('z');
+print_fizz();
 }
 else if (i % 5 == 0)
 {
-_putchar('B');
-_putchar('u');
-_putchar('z');
-_putchar('z');
+print_buzz();
 }
 else if (i > 9)
 {
