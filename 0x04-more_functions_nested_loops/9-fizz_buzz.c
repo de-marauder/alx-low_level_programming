2f7174439d45e4282e58f9f3d7a768992a37e68e
de-marauder <ezikegodson@gmail.com>
2022-07-07 15:06:42 -0700

    conditional printing of numbers 1-100

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,20 @@
 #include "main.h"
 
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+  return (write(1, &c, 1));
+}
+
+
 /**
  * main - print 1 - 100. Perform special operation for multiples of 3 and 5
  * Return: 0 always
