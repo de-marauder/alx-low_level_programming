8e6d987de44e31b1c9d362dab05a5546f684f0e2
de-marauder <ezikegodson@gmail.com>
2022-07-07 07:11:36 -0700

    check upper case

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: character to check
+ * Return: 1 if true, 0 if false
+ */
+
+int _isupper(int c);
+
+#endif
