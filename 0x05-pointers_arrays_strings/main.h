f964bd7ec86a63f5cb6a711bb08a90c20bc1ede4
de-marauder <ezikegodson@gmail.com>
2022-07-12 12:50:59 -0700

    switch values of variables with pointers

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,6 +3,16 @@
 
 
 /**
+ * swap_int - swaps the values of two integers
+ * @a: integer
+ * @b: integer
+ * Return: void
+ */
+
+void swap_int(int *a, int *b);
+
+
+/**
  * reset_to_98 - Updates a parameter using a pointer
  * @n: pointer to a variable @n
  * return: void
