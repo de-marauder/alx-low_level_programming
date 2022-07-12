e6934732f027b237067595d2ea6a7f1a1bac3095
de-marauder <ezikegodson@gmail.com>
2022-07-12 12:38:52 -0700

    Reset a variables value using pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Updates a parameter using a pointer
+ * @n: pointer to a variable @n
+ * return: void
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+/**
+ * reset_to_98 - Updates a parameter using a pointer
+ * @n: pointer to a variable @n
+ * return: void
+ */
+
+void reset_to_98(int *n);
+
+
+#endif
