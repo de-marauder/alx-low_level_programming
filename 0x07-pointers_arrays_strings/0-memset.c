ef6983f8bc66dd397ccbbb992d050cc00a12773c
de-marauder <ezikegodson@gmail.com>
2022-07-18 07:32:25 -0700

    overwrite values in memory

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,10 +10,10 @@
 
 char *_memset(char *s, char b, int n)
 {
-  int i;
-  for (i = 0; i < n; i++)
-    {
-      s[i] = b;
-    }
-  return (s);
+int i;
+for (i = 0; i < n; i++)
+{
+s[i] = b;
+}
+return (s);
 }
