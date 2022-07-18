3c70f6495e5934cff6baa14dc7300339d6cd60e9
de-marauder <ezikegodson@gmail.com>
2022-07-18 07:17:42 -0700

    overwrite values in memory

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - fills a specified memory location with a constant byte
+ * @s: buffer
+ * @b: overwriting character
+ * @n: number of memory spaces to be overwitten
+ * Return: edited buffer
+ */
+
+char *_memset(char *s, char b, int n)
+{
+  int i;
+  for (i = 0; i < n; i++)
+    {
+      s[i] = b;
+    }
+  return (s);
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+/**
+ * _memset - convers the values of a given memory location to a constant
+ * @s: buffer
+ * @b: overwriting character
+ * @n: number of spaces to im memory to overwrite
+ * Return: edited buffer
+ */
+
+char *_memset(char *s, char b, int n);
+
+#endif
